ca01ca65bc9081f206a6600ec33ccbf4042f2c35
tareque-bin-habib <shad.tbh@gmail.com>
2022-12-01 18:43:30 +0600

    setup router

--- src/App.js
+++ src/App.js
@@ -2,6 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Products from './components/Products/Products';
+import About from './components/About/About';
+import LogIn from './components/LogIn/LogIn';
+import Register from './components/Register/Register';
 
 function App() {
   const router = createBrowserRouter([
@@ -9,7 +14,26 @@ function App() {
       path: '/',
       element: <Main></Main>,
       children: [
-
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/products',
+          element: <Products></Products>
+        },
+        {
+          path: '/about',
+          element: <About></About>
+        },
+        {
+          path: '/login',
+          element: <LogIn></LogIn>
+        },
+        {
+          path: '/register',
+          element: <Register></Register>
+        }
       ]
     }
   ])
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,29 +2,21 @@ import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <Navbar bg="light" expand="lg">
+        <Navbar bg="light" expand="lg" className='p-2'>
             <Container>
-                <Navbar.Brand className='fw-bold' href="#home">Multi <span className='vendor'>Vendor</span> </Navbar.Brand>
+                <Navbar.Brand href="#home"><h3 className='fw-bold'>Multi <span className='vendor'>Vendor</span></h3> </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto ">
-                        <Nav.Link href="#home">Home</Nav.Link>
-                        <Nav.Link href="#link">Link</Nav.Link>
-                        <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-                            <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.2">
-                                Another action
-                            </NavDropdown.Item>
-                            <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                            <NavDropdown.Divider />
-                            <NavDropdown.Item href="#action/3.4">
-                                Separated link
-                            </NavDropdown.Item>
-                        </NavDropdown>
+                    <Nav className="mx-auto me-0 menu ">
+                        <Link to='/'>Home</Link>
+                        <Link to='/products'>Products</Link>
+                        <Link to='/about'>About</Link>
+                        <Link to='/login'>Login</Link>
+                        <Link to='/register'>Register</Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
